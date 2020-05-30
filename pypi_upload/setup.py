35235325b2f4c0783d9c86e8b9934464f3c66211
Tandoh <anthony.tandoh@tanacom.io>
2020-05-30 13:02:40 +0000

    commit Putting Code on PyPi

---
+++ pypi_upload/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tanacom_distributions',
+      version='0.1',
+      description='A test case of Gaussian distributions',
+      packages=['distributions'],
+      author='Anthony Tandoh',
+      author_email= 'anthony.tandog@tanacom.io',
+      zip_safe=False)
